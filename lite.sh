1d8c5f60bd7bb7ffe661add589591e8b4e902b2e
putrahss <141410006+putrahss@users.noreply.github.com>
2025-02-23 02:02:07 +0800

    Update lite.sh

--- lite.sh
+++ lite.sh
@@ -24,7 +24,7 @@ green='\e[0;32m'
 ipsaya=$(wget -qO- ipinfo.io/ip)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/bagusid93/hss/main/sc3"
+data_ip="https://raw.githubusercontent.com/rajaganjil93/ganjil/main/ip"
 checking_sc() {
   useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
   if [[ $date_list < $useexp ]]; then
